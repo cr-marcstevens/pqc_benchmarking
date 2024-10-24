887f4bd76490e2a9e83ab959ccb60b9c7e1b3f52
cr-marcstevens <github@marc-stevens.nl>
2024-10-24 12:00:21 +0200

    speed_sig_stfl PR is now merged into liboqs. Various tweaks to bench_oqs.sh script.

--- bench_oqs.sh
+++ bench_oqs.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-OQSREPO="https://github.com/cr-marcstevens/liboqs" # contains speed_sig_stfl
-#OQSREPO="https://github.com/open-quantum-safe/liboqs"
+OQSREPO="https://github.com/open-quantum-safe/liboqs"
 
 OQSOPTIONS="-DOQS_ENABLE_SIG_STFL_XMSS=ON -DOQS_ENABLE_SIG_STFL_LMS=ON -DOQS_HAZARDOUS_EXPERIMENTAL_ENABLE_SIG_STFL_KEY_SIG_GEN=ON -DOQS_DIST_BUILD=OFF"
 
-# liboqs/build/tests/speed_kem --algs
-#OQSKEMALGS="BIKE-L5 HQC-256 ML-KEM-1024 FrodoKEM-1344-AES FrodoKEM-1344-SHAKE Classic-McEliece-6688128 Classic-McEliece-6688128f"
-
-# liboqs/build/tests/speed_sig --algs
-#OQSDSSALGS="ML-DSA-87 Falcon-1024 Falcon-padded-1024 SPHINCS+-SHA2-256f-simple SPHINCS+-SHA2-256s-simple SPHINCS+-SHAKE-256f-simple SPHINCS+-SHAKE-256s-simple"
-
-# liboqs/build/tests/speed_sig_stfl --algs
-#OQSDSSSTFLALGS="XMSSMT-SHA2_20/2_256 XMSSMT-SHA2_20/4_256 XMSSMT-SHAKE_20/2_256 XMSSMT-SHAKE_20/4_256"
+STFLBENCHOPTIONS="-d 1000 -i"
 
 BENCHOPTIONS="-d 10 -i"
 
@@ -31,10 +23,6 @@ if [ -z $(command -v git) ]; then
 	echo "Error: git not found"
 	exit 1
 fi
-if [ -z $(command -v openssl) ]; then
-	echo "Error: openssl not found"
-	exit 2
-fi
 
 if [ ! -d liboqs ]; then
 	echo -n "* Retreiving libOQS repository ($OQSREPO)... "
@@ -51,14 +39,21 @@ else
 	if [ `git log HEAD..origin/main --oneline | wc -l` -eq 0 ]; then
 		echo "[OK]"
 	else
+		git checkout src/oqsconfig.h.cmake
 		if git pull 2>/dev/null >/dev/null; then
 			echo "[OK]"
 		else
 			echo "[Failed]"
 			exit 4
 		fi
-		# only remove build if there is an update
 		rm -rf build 2>/dev/null >/dev/null
+		rm $HOSTNAME/{kem,sig}_*.log
+
+		for d in OQS_ENABLE_SIG_STFL_lms_sha256_h20_w1 OQS_ENABLE_SIG_STFL_lms_sha256_h20_w2 OQS_ENABLE_SIG_STFL_lms_sha256_h20_w4 OQS_ENABLE_SIG_STFL_lms_sha256_h20_w8 ; do
+			if [ `grep $d src/oqsconfig.h.cmake | wc -l` -eq 0 ]; then
+				echo "#cmakedefine $d 1" >> src/oqsconfig.h.cmake
+			fi
+		done
 	fi
 	cd ..
 fi
@@ -94,55 +89,70 @@ echo "* Building..."
 make -j 8 || exit 10
 
 
+
+# liboqs/build/tests/speed_kem --algs
+OQSKEMALGS=`tests/speed_kem --algs | grep -v disabled | tr '\n' ' '`
+
+# liboqs/build/tests/speed_sig --algs
+OQSDSSALGS=`tests/speed_sig --algs | grep -v disabled | tr '\n' ' '`
+
+# liboqs/build/tests/speed_sig_stfl --algs
+OQSDSSSTFLALGS=`tests/speed_sig_stfl --algs | grep -v disabled | tr '\n' ' '`
+
+echo "KEM      : $OQSKEMALGS"
+echo "SIG      : $OQSDSSALGS"
+echo "SIG-STFL : $OQSDSSSTFLALGS"
+
+echo "Benchmarking starts in 10 seconds..."
+sleep 10
+
 ############### BENCHMARK PQC KEMS #####################
 
-LOGFILE=../../$HOSTNAME/kem.log
-[ -f $LOGFILE ] && rm $LOGFILE
-
-if [ -z $OQSKEMALGS ]; then
+LOGBASE=../../$HOSTNAME/kem
+if [ -z "$OQSKEMALGS" ]; then
 	echo "* Benchmarking..."
-	$STDBUF ./tests/speed_kem $BENCHOPTIONS | tee -a $LOGFILE
+	if [ ! -f $LOGBASE.log ]; then
+		$STDBUF ./tests/speed_kem $BENCHOPTIONS |& tee $LOGBASE.log
+	fi
 else
 	for a in $OQSKEMALGS; do
-		echo -n "* Benchmarking $a..."
-		echo "=============================== $a ==============================" >> $LOGFILE
-		./tests/speed_kem $BENCHOPTIONS $a >> $LOGFILE
-		echo -e "\n\n" >> $LOGFILE
-		echo "[Done]"
+		LOGFILE=${LOGBASE}_${a}.log
+		if [ -f $LOGFILE ]; then continue; fi
+		echo "* Benchmarking $a..."
+		./tests/speed_kem $BENCHOPTIONS $a |& tee $LOGFILE
 	done
 fi
 
 ############### BENCHMARK PQC DSAS #####################
 
-LOGFILE=../../$HOSTNAME/dss.log
-[ -f $LOGFILE ] && rm $LOGFILE
-if [ -z $OQSDSSALGS ]; then
+LOGBASE=../../$HOSTNAME/sig
+if [ -z "$OQSDSSALGS" ]; then
 	echo "* Benchmarking..."
-	$STDBUF ./tests/speed_sig $BENCHOPTIONS | tee -a $LOGFILE
+	if [ ! -f $LOGBASE.log ]; then
+		$STDBUF ./tests/speed_sig $BENCHOPTIONS |& tee $LOGBASE.log
+	fi
 else
 	for a in $OQSDSSALGS; do
-		echo -n "* Benchmarking $a..."
-		echo "=============================== $a ==============================" >> $LOGFILE
-		./tests/speed_sig $BENCHOPTIONS $a >> $LOGFILE
-		echo -e "\n\n" >> $LOGFILE
-		echo "[Done]"
+		LOGFILE=${LOGBASE}_${a}.log
+		if [ -f $LOGFILE ]; then continue; fi
+		echo "* Benchmarking $a..."
+		./tests/speed_sig $BENCHOPTIONS $a |& tee $LOGFILE
 	done
 fi
 
 ############### BENCHMARK PQC Stateful DSAS #####################
 
-LOGFILE=../../$HOSTNAME/dss_stfl.log
-[ -f $LOGFILE ] && rm $LOGFILE
-
-if [ -z $OQSDSSSTFLALGS ]; then
+LOGBASE=../../$HOSTNAME/sig_stfl
+if [ -z "$OQSDSSSTFLALGS" ]; then
 	echo "* Benchmarking..."
-	$STDBUF ./tests/speed_sig_stfl $BENCHOPTIONS |& tee -a $LOGFILE
+	if [ ! -f $LOGBASE.log ]; then
+		$STDBUF ./tests/speed_sig_stfl $BENCHOPTIONS |& tee $LOGBASE.log
+	fi
 else
 	for a in $OQSDSSSTFLALGS; do
-		echo -n "* Benchmarking $a..."
-		echo "=============================== $a ==============================" >> $LOGFILE
-		./tests/speed_sig_stfl $BENCHOPTIONS $a >> $LOGFILE
-		echo -e "\n\n" >> $LOGFILE
-		echo "[Done]"
+		LOGFILE=${LOGBASE}_${a}.log
+		if [ -f $LOGFILE ]; then continue; fi
+		echo "* Benchmarking $a..."
+		./tests/speed_sig_stfl $BENCHOPTIONS $a |& tee $LOGFILE
 	done
 fi
